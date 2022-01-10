d547a80d9ffeb996f848803a19ae862adb5f2f8c
Nanna Sagen <82771340+nannasagen@users.noreply.github.com>
2022-01-10 13:03:09 +0100

    Add files via upload

---
+++ Api.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h3>Brewdog beer</h3>
+   
+    <ul v-for="beer in beerList" v-bind:key="beer.id">
+        <li>{{beer.name}}</li>
+         <li>{{beer.image_url}}</li>
+         <li>{{beer.abv}}</li>
+        <li>{{beer.description}}</li>
+        
+
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Api",
+  data() {
+    return {
+      beerList: [],
+    };
+  },
+
+  async mounted() {
+    let response = await fetch("https://api.punkapi.com/v2/beers");
+    let data = await response.json();
+    this.beerList.push(...data);
+    console.log(data);
+    return data;
+  },
+};
+</script>
---
+++ App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+   
+    <Api></Api>
+  </div>
+</template>
+
+<script>
+import Api from './components/Api.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Api
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+body{
+  background-color:darkslategrey;
+}
+h3{
+  font-family: 'Courier New', Courier, monospace;
+  font-size: 40px;
+  color : rgb(92, 49, 18);
+}
+li.nameTag{
+  font-family:'Courier New', Courier, monospace;
+  font-size: 45px;
+  color: teal;
+}
+li{
+  font-family: 'Courier New', Courier, monospace;
+  font-size: 20px;
+  color:peru;
+}
+</style>
